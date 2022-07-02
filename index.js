babbd1e7ec339723f0d552760d73618f663b9456
khushalpokar <khushalpokar9@gmail.com>
2022-07-02 19:35:21 +0530

    star

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-let arr = [1, 2, 3, 6, 7, 8, 3, 6];
-let obj = {};
+// let arr = [1, 2, 3, 6, 7, 8, 3, 6];
+// let obj = {};
 
-for(let i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
-    obj[arr[i]] = arr[i];
+// for(let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+//     obj[arr[i]] = arr[i];
+// }
+
+// console.log(Object.keys(obj));
+
+star = "";
+
+for(let i = 1; i <= 5; i++) {
+    for(let j = 0; j < i; j++) {
+        star += "*";
+    }
+    star += "\n";
 }
 
-console.log(Object.keys(obj));
+console.log(star);
